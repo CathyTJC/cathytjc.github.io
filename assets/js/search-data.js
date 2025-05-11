dc49297e10af5c80da8a09789ba1d2b517aabe80
CathyTJC <56180667+CathyTJC@users.noreply.github.com>
2025-05-11 01:22:06 +0000

    Deploying to gh-pages from @ CathyTJC/cathytjc.github.io@eb83f47362f0004c052c8cab8d729ec7165c17be 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,28 +30,14 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
+        },{id: "nav-group",
+          title: "group",
           description: "members of the lab or group",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
